a1d16ef73dd59a37f082294d6000c1533240d78e
hoan-blip <hoqhoan6868@gmailcom>
2021-05-12 14:06:01 +0700

    Nộp bài

--- 05/TriangleClass/index.php
+++ 05/TriangleClass/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>[**Bài tập] Thiết kế và triển khai lớp Triangle</title>
+    <title>Thiết kế và triển khai lớp Triangle</title>
 </head>
 <body>
     <?php
